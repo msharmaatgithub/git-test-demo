eee54efc9364a648371ca82e43f4bde188e04fbc
Manoj Sharma <manoj.ajmer@gmail.com>
2023-10-28 20:40:01 +0530

    Comment added and button text updated

--- Main.java
+++ Main.java
@@ -14,13 +14,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button button = new Button("Click Me!");
-        button.setOnAction(event -> System.out.println("Button Not clicked!"));
+        button.setOnAction(event -> System.out.println("Button clicked!"));
 
         StackPane root = new StackPane();
         root.getChildren().add(button);
 
         Scene scene = new Scene(root, 300, 200);
 
+        //This is a comment for testing purpose
         primaryStage.setTitle("JavaFX Application For Testing");
         primaryStage.setScene(scene);
         primaryStage.show();
